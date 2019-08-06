d0e9ebad88a6c40f5c44b64a0c6752150a76b8e1
DharaniRagam <35969669+DharaniRagam@users.noreply.github.com>
2019-08-06 12:46:44 +0530

    import export components

---
+++ seperatecompfiles/MyInfo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function MyInfo(){
+  return(
+    <div>
+    <h1> Sample data</h1>
+      <ol>
+        <li>dharani</li>
+        <li>ragam</li>
+        <li>sam</li>
+      </ol>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default MyInfo
